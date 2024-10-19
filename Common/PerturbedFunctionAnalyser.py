380289bd1d39fd9d858108b73dbd4eae4cc874d6
JP <james@mail.com>
2024-10-19 03:34:09 +0100

    get 3d visualisation working

--- Common/PerturbedFunctionAnalyser.py
+++ Common/PerturbedFunctionAnalyser.py
@@ -1,31 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpmath import mp, mpf, sin
+from mpmath import mp
 from typing import Callable, Optional, Tuple
-from Common.FloatingPointAnalyser import FloatingPointAnalyzer
+from mpl_toolkits.mplot3d import Axes3D  # For 3D plotting
+from Common.FloatingPointAnalyser import FloatingPointAnalyser
 
-### wip
-class PerturbedFunctionAnalyer:
+# Include your existing FloatingPointAnalyser class here
+
+class FunctionPerturbationAnalyser:
     def __init__(self, 
-                 a_min: float, 
-                 a_max: float, 
-                 num_a: int, 
-                 b_min: float, 
-                 b_max: float, 
-                 num_b: int, 
+                 func_template: Callable, 
+                 arg_types: Tuple[str, ...],
                  x_min: float, 
                  x_max: float, 
                  num_x: int = 1000, 
                  k_min: float = 1.0, 
-                 k_max: float = 10.0, 
+                 k_max: float = 100.0, 
                  num_k: int = 1000, 
                  sample_size: int = 100, 
                  relative: bool = False, 
-                 precision: int = 50):
-        self.a_values = np.linspace(a_min, a_max, num_a)
-        self.b_values = np.linspace(b_min, b_max, num_b)
-        self.num_a = num_a
-        self.num_b = num_b
+                 precision: int = 50,
+                 constant_values: Optional[Tuple[float, ...]] = None,
+                 a_min: float = 1.0,
+                 a_max: float = 1.0,
+                 num_a: int = 1,
+                 b_min: float = 0.0,
+                 b_max: float = 0.0,
+                 num_b: int = 1):
+        self.func_template = func_template
+        self.arg_types = arg_types
         self.x_min = x_min
         self.x_max = x_max
         self.num_x = num_x
@@ -35,17 +38,36 @@ class PerturbedFunctionAnalyer:
         self.sample_size = sample_size
         self.relative = relative
         self.precision = precision
+        self.constant_values = constant_values
+        self.a_min = a_min
+        self.a_max = a_max
+        self.num_a = num_a
+        self.b_min = b_min
+        self.b_max = b_max
+        self.num_b = num_b
+
+        # Generate values for a and b
+        self.a_values = np.linspace(a_min, a_max, num=num_a)
+        self.b_values = np.linspace(b_min, b_max, num=num_b)
         mp.dps = self.precision
-        self.hypervolume = np.zeros((num_a, num_b, num_k, num_x))
-    
+
     def run_analysis(self):
-        for i, a in enumerate(self.a_values):
-            for j, b in enumerate(self.b_values):
-                def func(x):
-                    return a * sin(x) + b
-                analyzer = FloatingPointAnalyzer(
-                    func=func,
-                    arg_types=('vary',),
+        num_samples = self.sample_size
+        num_k = self.num_k
+        num_a = self.num_a
+        num_b = self.num_b
+        perturbations_array = np.zeros((num_b, num_a, num_samples, num_k))
+        differences_array = np.zeros((num_b, num_a, num_samples, num_k))
+        
+        for b_idx, b in enumerate(self.b_values):
+            for a_idx, a in enumerate(self.a_values):
+                print(f"Processing a={a}, b={b}")
+                # Define a function that only takes x as input
+                def func_a_b(x):
+                    return self.func_template(x, a, b)
+                analyser = FloatingPointAnalyser(
+                    func=func_a_b,
+                    arg_types=self.arg_types,
                     x_min=self.x_min,
                     x_max=self.x_max,
                     num_x=self.num_x,
@@ -54,32 +76,40 @@ class PerturbedFunctionAnalyer:
                     num_k=self.num_k,
                     sample_size=self.sample_size,
                     relative=self.relative,
-                    precision=self.precision
+                    precision=self.precision,
+                    constant_values=self.constant_values
                 )
-                perturbations, diffs = analyzer.compute_differences()
-                self.hypervolume[i, j, :, :] = diffs
-                print(f"Analyzed a={a}, b={b}")
-    
-    def plot_hypervolume(self, b_samples_indices: Optional[list] = None):
-        if b_samples_indices is None:
-            b_samples_indices = np.linspace(0, self.num_b -1, 4, dtype=int).tolist()
-        fig = plt.figure(figsize=(20, 15))
-        for idx, b_idx in enumerate(b_samples_indices):
-            ax = fig.add_subplot(2, 2, idx+1, projection='3d')
-            a_vals, k_vals = np.meshgrid(self.a_values, np.linspace(self.k_min, self.k_max, self.num_k))
-            a_vals = a_vals.flatten()
-            k_vals = k_vals.flatten()
-            diffs = self.hypervolume[b_idx, :, :].flatten()
-            scatter = ax.scatter(k_vals, a_vals, diffs, c=diffs, cmap='viridis', marker='o', s=1)
-            ax.set_title(f'b={self.b_values[b_idx]:.2f}')
-            ax.set_xlabel('k * ULP(x)')
-            ax.set_ylabel('a')
-            ax.set_zlabel('Differences' if not self.relative else 'Relative Differences')
-            ax.set_xscale('log')
-            ax.set_yscale('log')
-            ax.set_zscale('log')
-            fig.colorbar(scatter, ax=ax, shrink=0.5, aspect=5)
-        plt.tight_layout()
-        plt.show()
+                perturbations, differences = analyser.compute_differences()
+                # Store data
+                perturbations_array[b_idx, a_idx, :, :] = perturbations
+                differences_array[b_idx, a_idx, :, :] = differences
+        self.perturbations_array = perturbations_array
+        self.differences_array = differences_array
 
-
+    def plot_differences(self):
+        # For 4 samples of b
+        num_b_samples = min(4, self.num_b)
+        b_indices = np.linspace(0, self.num_b - 1, num_b_samples, dtype=int)
+        for b_idx in b_indices:
+            b = self.b_values[b_idx]
+            fig = plt.figure(figsize=(12, 8))
+            ax = fig.add_subplot(111, projection='3d')
+            for a_idx, a in enumerate(self.a_values):
+                # Sample a subset of indices to avoid overplotting
+                sample_indices = np.linspace(0, self.sample_size -1, max(1, self.sample_size // 10), dtype=int)
+                for idx in sample_indices:
+                    perturbations = self.perturbations_array[b_idx, a_idx, idx, :]
+                    differences = self.differences_array[b_idx, a_idx, idx, :]
+                    non_zero = differences > 0
+                    if np.any(non_zero):
+                        ax.plot(
+                            np.log10(perturbations[non_zero]),
+                            np.full_like(perturbations[non_zero], a),
+                            np.log10(differences[non_zero]),
+                            '.', alpha=0.5
+                        )
+            ax.set_title(f'Differences for b={b}')
+            ax.set_xlabel('log10(Perturbations)')
+            ax.set_ylabel('Multiplier a')
+            ax.set_zlabel('log10(Differences)')
+            plt.show()
--- Uses/Sin3d.py
+++ Uses/Sin3d.py
@@ -1,27 +1,42 @@
-from Common.PerturbedFunctionAnalyser import PerturbedFunctionAnalyser
+from Common.PerturbedFunctionAnalyser import FunctionPerturbationAnalyser
 from mpmath import sin
+from mpmath import mp
+import numpy as np
 
-def sin_func(x):
-    return sin(x)
+# Define the function template
+def func_template(x, a, b):
+    if isinstance(x, mp.mpf):
+        # Convert a and b to mpf types
+        a_mp = mp.mpf(a)
+        b_mp = mp.mpf(b)
+        return a_mp * mp.cos(x) + b_mp
+    else:
+        return a * np.cos(x) + b
 
-analyzer = PerturbedFunctionAnalyser(
-    a_min=0.5,
-    a_max=1.5,
-    num_a=10,
-    b_min=-0.5,
-    b_max=0.5,
-    num_b=10,
-    x_min=1e-5,
-    x_max=1e5,
+# Set up the analyzer
+analyzer = FunctionPerturbationAnalyser(
+    func_template=func_template,
+    arg_types=('vary',),  # We are varying x
+    x_min=1e-10,          # Set your desired x range
+    x_max=1e+10,
     num_x=1000,
-    k_min=1,
-    k_max=10,
+    k_min=1.0,
+    k_max=100.0,
     num_k=1000,
     sample_size=100,
-    relative=True,
+    relative=False,
     precision=50,
-    turn_it_up_to=11
+    a_min=0.5,
+    a_max=2.0,
+    num_a=10,     # Number of samples for a
+    b_min=-1.0,
+    b_max=1.0,
+    num_b=4       # Number of samples for b
 )
 
+# Run the analysis
 analyzer.run_analysis()
-analyzer.plot_hypervolume()
+
+# Plot the results
+analyzer.plot_differences()
+
